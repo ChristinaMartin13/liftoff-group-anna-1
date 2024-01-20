09e4e38d62660451ede0d8cfff6e9298e02e79c6
Emily Kemlage <emilykemlage@gmail.com>
2024-01-19 22:51:28 -0600

    Edited Register HTML file to add a place for Name in the registration form. Edited input model class to validate input and assign it to a variable. Edited OnPostAsync to store input in database.

--- option4mvc/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ option4mvc/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -71,6 +71,10 @@ namespace option4mvc.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
+            [Required]
+            [StringLength(100, ErrorMessage = "Please enter your full name", MinimumLength = 3)]
+            [Display(Name = "Name")]
+            public string Name { get; set; }
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
@@ -115,6 +119,7 @@ namespace option4mvc.Areas.Identity.Pages.Account
             {
                 var user = CreateUser();
 
+                user.Name = Input.Name; 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
